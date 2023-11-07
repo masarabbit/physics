322bf0fcb7da17a383c15cba9bb177b93ffd71b6
masarabbit <masahitoart@gmail.com>
2023-11-07 23:36:46 +0100

    crane_game: set up base code

--- crane_game/app.js
+++ crane_game/app.js
@@ -2,24 +2,14 @@
 function init() { 
 
   const settings = {
-    capsuleNo: 20,
-    isTurningHandle: false,
-    isHandleLocked: false,
-    handlePrevDeg: 0,
-    handleDeg: 0,
-    handleRotate: 0,
+    capsuleNo: 5,
     flapRotate: 0,
-    collectedNo: 0,
   }
 
   const elements = {
     wrapper: document.querySelector('.wrapper'),
-    capsuleMachine: document.querySelector('.capsule-machine'),
-    shakeButton: document.querySelector('.shake'),
-    seeInsideButton: document.querySelector('.see-inside'),
-    circle: document.querySelector('.circle'),
-    handle: document.querySelector('.handle'),
-    toyBox: document.querySelector('.toy-box'),
+    machine: document.querySelector('.machine'),
+    indicator: document.querySelector('.indicator'),
   }
 
   const vector = {
@@ -77,10 +67,10 @@ function init() {
   const radToDeg = rad => Math.round(rad * (180 / Math.PI))
   const angleTo = ({ a, b }) => Math.atan2(b.y - a.y, b.x - a.x)
   const distanceBetween = (a, b) => Math.round(Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2)))
-  const getPage = (e, type) => e.type[0] === 'm' ? e[`page${type}`] : e.touches[0][`page${type}`]
-  const calcCollectedX = () => settings.collectedNo % 10 * 32
-  const calcCollectedY = () => Math.floor(settings.collectedNo / 10) * 32
-  const nearest360 = n => n === 0 ? 0 : (n - 1) + Math.abs(((n - 1) % 360) - 360)
+  // const getPage = (e, type) => e.type[0] === 'm' ? e[`page${type}`] : e.touches[0][`page${type}`]
+  // const calcCollectedX = () => settings.collectedNo % 10 * 32
+  // const calcCollectedY = () => Math.floor(settings.collectedNo / 10) * 32
+  // const nearest360 = n => n === 0 ? 0 : (n - 1) + Math.abs(((n - 1) % 360) - 360)
 
   const setStyles = ({ el, x, y, w, deg }) =>{
     if (w) el.style.width = w
@@ -112,20 +102,23 @@ function init() {
   ]
 
 
-  const getRandomToy = () => {
-    return ['bunny', 'duck-yellow', 'duck-pink', 'star', 'water-melon', 'panda', 'dino', 'roboto-san', 'roboto-sama', 'penguin', 'turtle'][randomN(11) - 1]
-  }
 
-  new Array(settings.capsuleNo).fill('').forEach(() => {
-    const capsule = Object.assign(document.createElement('div'), 
-      { className: 'capsule-wrapper pix',
-        innerHTML: `<div class="capsule">
-                      <div class="lid"></div>
-                      <div class="${getRandomToy()} toy pix"></div>
-                      <div class="base ${['red', 'pink', 'white', 'blue'][randomN(4) - 1]}"></div>
-                    </div>`
-    })
-    elements.capsuleMachine.appendChild(capsule)
+  new Array(settings.capsuleNo).fill('').forEach((_, i) => {
+    const toy = Object.assign(document.createElement('div'), 
+    { className: 'toy',
+      innerHTML: `
+      <div class="capsule-wrapper pix" data-id="${i}">
+        <div class="capsule"></div>
+      </div>
+      <div class="capsule-wrapper pix" data-id="${i}">
+        <div class="capsule"></div>
+      </div>
+      <div class="capsule-wrapper pix" data-id="${i}">
+        <div class="capsule"></div>
+      </div>
+      `
+  })
+    elements.machine.appendChild(toy)
   })
   lineData.forEach(() => {
     ;[
@@ -133,24 +126,15 @@ function init() {
         { className: 'line-start', innerHTML: '<div class="line"></div>'}),
       Object.assign(document.createElement('div'), { className: 'line-end' })
     ].forEach(ele => {
-      elements.capsuleMachine.appendChild(ele)
+      elements.machine.appendChild(ele)
     })
   })
 
   const lineStarts = document.querySelectorAll('.line-start')
   const lines = document.querySelectorAll('.line')
   const lineEnds = document.querySelectorAll('.line-end')
-  const toys = document.querySelectorAll('.toy')
-  const { width: capsuleMachineWidth, height: capsuleMachineHeight } = elements.capsuleMachine.getBoundingClientRect()
+  const { width: machineWidth, height: machineHeight } = elements.machine.getBoundingClientRect()
 
-  const handleAxis = () => {
-    const { left: handleX, top: handleY } = elements.circle.getBoundingClientRect()
-    const { top, left } = elements.capsuleMachine.getBoundingClientRect()
-    return {
-      x: handleX - left + 80,
-      y: handleY - top + 80
-    }
-  }
 
   const updateLines = () => {
     lineData.forEach((l, i) => {
@@ -175,18 +159,17 @@ function init() {
       el: c,
       id: i,
       deg: 0,
-      radius: 36, // actual radius should be 32, but setting it higher
-      bounce: -0.3, // this reduces the velocity gradually
+      radius: 16, // actual radius should be 32, but setting it higher
+      bounce: -0.5, // this reduces the velocity gradually
       friction: 0.99,
-      toy: toys[i]
     }
 
     data.velocity = data.create(0, 1)  //? velocity is another vector
     data.velocity.setLength(10)
     data.velocity.setAngle(degToRad(90))
     data.setXy({
-      x: randomN(capsuleMachineWidth - 32), 
-      y: randomN(capsuleMachineHeight - 250), 
+      x: randomN(machineWidth - 32), 
+      y: randomN(machineHeight - 250), 
     })
 
     // gravity
@@ -207,16 +190,6 @@ function init() {
     }
   }
 
-  const shake = () => {
-    capsuleData.forEach(c => {
-      c.velocity.setAngle(degToRad(randomN(270)))
-      c.velocity.setXy({ x: 10, y: 10})        
-      c.accelerate(c.acceleration)
-    })
-    elements.capsuleMachine.classList.add('shake')
-    setTimeout(()=> elements.capsuleMachine.classList.remove('shake'), 500)
-
-  }
 
   const rotateLines = angles => {
     angles.forEach((angle, i) => {
@@ -262,47 +235,11 @@ function init() {
     }
   }
 
-  const release = () => {
-    settings.flapRotate = 0
-    settings.isHandleLocked = true
-    setTimeout(openFlap, 30)
-  }
-
-  capsuleData.forEach(c => {
-    c.el.addEventListener('click', ()=> {
-      const { width: bodyWidth, height: bodyHeight } = elements.wrapper.getBoundingClientRect()
-      const { top, left } = elements.capsuleMachine.getBoundingClientRect()
-      const { left: toyBoxLeft, top: toyBoxTop } = elements.toyBox.getBoundingClientRect()
-
-        elements.wrapper.classList.add('lock')
-        c.el.classList.add('enlarge')
-        c.selected = true
-
-        setStyles({
-          el : c.el,
-          x: (bodyWidth / 2) - left,
-          y: (bodyHeight / 2) - top,
-          deg: nearest360(c.deg)
-        })
-        setStyles({ el: c.toy, deg: 0 })
-        setTimeout(()=> c.el.classList.add('open'), 700)
-        setTimeout(()=> {
-          elements.wrapper.classList.remove('lock')
-          c.toy.classList.add('collected')
-          setStyles({
-            el : c.el,
-            x: toyBoxLeft - left + 16 + calcCollectedX(),
-            y: toyBoxTop - top + 16 + calcCollectedY(),
-          })
-          settings.collectedNo++
-        }, 1800)
-    })
-    setStyles(c)
-  })
 
   const spaceOutCapsules = c => {
     capsuleData.forEach(c2 =>{
-      if (c.id === c2.id || c2.selected) return
+      // console.log('test', c.el.dataset.id )
+      if (c.el.dataset.id === c2.el.dataset.id || c2.selected) return
       const distanceBetweenCapsules = distanceBetween(c, c2)
       if (distanceBetweenCapsules < (c.radius * 2)) {
         c.velocity.multiplyBy(-0.6)
@@ -346,18 +283,18 @@ function init() {
     })
   }
 
-  const hitCheckCapsuleMachineWalls = c => {
+  const hitCheckMachineWalls = c => {
     const buffer = 5
-    if (c.x + c.radius + buffer > capsuleMachineWidth) {
-      c.x = capsuleMachineWidth - (c.radius + buffer)
+    if (c.x + c.radius + buffer > machineWidth) {
+      c.x = machineWidth - (c.radius + buffer)
       c.velocity.x = c.velocity.x * c.bounce
     }
     if (c.x - (c.radius + buffer) < 0) {
       c.x = c.radius
       c.velocity.x = c.velocity.x * c.bounce
     }
-    if (c.y + c.radius + buffer > capsuleMachineHeight) {
-      c.y = capsuleMachineHeight - c.radius - buffer
+    if (c.y + c.radius + buffer > machineHeight) {
+      c.y = machineHeight - c.radius - buffer
       c.velocity.y = c.velocity.y * c.bounce
     }
     if (c.y - c.radius < 0) {
@@ -379,7 +316,7 @@ function init() {
 
       spaceOutCapsules(c)
       hitCheckLines(c)
-      hitCheckCapsuleMachineWalls(c)
+      hitCheckMachineWalls(c)
 
       if (Math.abs(c.prevX - c.x) < 2 && Math.abs(c.prevY - c.y) < 2) {
         c.velocity.setXy({ x: 0, y: 0 })
@@ -387,10 +324,6 @@ function init() {
       } else {
         if (Math.abs(c.prevX - c.x)) {
           // rotate capsule
-          setStyles({
-            el: c.toy,
-            deg: c.deg + (c.x - c.prevX) * 2
-          })
           c.deg += (c.x - c.prevX) * 2
         }
       }
@@ -399,76 +332,6 @@ function init() {
   }
 
 
-  const grabHandle = e => {
-    if (settings.isHandleLocked) return
-    const { top, left } = elements.capsuleMachine.getBoundingClientRect()
-    settings.isTurningHandle = true
-    settings.handleDeg = radToDeg(angleTo({
-      a: {
-        x: getPage(e, 'X') - left,
-        y: getPage(e, 'Y') - top
-      },
-      b: handleAxis()
-    }))
-    settings.handleRotate = 0
-  }
-
-  const releaseHandle = () => {
-    settings.isTurningHandle = false
-    setStyles({
-      el: elements.handle,
-      deg: 0
-    })
-  }
-
-  const rotateHandle = e => {
-    if (!settings.isTurningHandle || settings.isHandleLocked) return
-    const { top, left } = elements.capsuleMachine.getBoundingClientRect()
-  
-    settings.prevHandleDeg = settings.handleDeg 
-    const deg = radToDeg(angleTo({
-      a: { x: getPage(e, 'X') - left, y: getPage(e, 'Y') - top },
-      b: handleAxis()
-    }))
-    settings.handleDeg = deg
-
-    const diff = settings.handleDeg - settings.prevHandleDeg
-    
-    if (diff >= 1) {
-      setStyles({
-        el: elements.handle,
-        deg: settings.handleRotate
-      })
-    }
-
-    if (diff > 0 && diff < 50) settings.handleRotate += diff
-    if (settings.handleRotate > 350) {
-      setStyles({
-        el: elements.handle,
-        deg: 10
-      })
-      release()
-      settings.isTurningHandle = false
-    }
-  }
-
-  ;['mousedown', 'touchstart'].forEach(action => {
-    elements.handle.addEventListener(action, grabHandle)
-  })
-
-  ;['mouseup', 'mouseleave', 'touchend'].forEach(action => {
-    elements.circle.addEventListener(action, releaseHandle)
-  })
-
-  ;['mousemove', 'touchmove'].forEach(action => {
-    window.addEventListener(action, rotateHandle) 
-  })
-
-  elements.shakeButton.addEventListener('click', shake)
-  elements.seeInsideButton.addEventListener('click', ()=> {
-    elements.capsuleMachine.classList.toggle('see-through')
-    elements.seeInsideButton.innerHTML = elements.capsuleMachine.classList.contains('see-through') ? 'hide' : 'see inside'
-  })
 
   updateLines()
   setInterval(animateCapsules, 30)
